4bbb09d378a5bde90fa7b6270b7e43077cc70da8
Deepak Muduli <deepakmuduli978@gmail.com>
2026-06-13 11:22:19 +0530

    Changes happened in contact and service page

--- src/menubar/Contact.jsx
+++ src/menubar/Contact.jsx
@@ -1,14 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Contact = () => {
+  const [name,setName]=useState("");
+  const [email,setEmail]=useState("");
+  const [phone, setPhone]=useState("");
+  const [password,setPassword]=useState("");
+
+  const handleSubmit= (e) =>{
+    e.preventDefault();
+
+    alert("Form Submitted Successfully.");
+    console.log("Name:"+name);
+    console.log("Email:"+email);
+    console.log("Phone_No:"+phone);
+    console.log("Password:"+password);
+
+
+  }
+
   return (
     <>
-    <form method='put'>
-      <div>Name:<input type="text" name='name'></input></div>
-    <div>Email:<input type="mail" name='email'></input></div>
-    <div>Phone:<input type="number" name='phone'></input> </div>
-    <div>password:<input type="password" name='password'></input></div>
-    <div><button type="submit">SPress  Ok</button>
+    <form onSubmit={handleSubmit}>
+      <div>Name:<input type="text" name='name' value={name} onChange={(e)=>setName(e.target.value)}></input></div>
+    <div>Email:<input type="email" name='email' value={email} onChange={(e)=>setEmail(e.target.value)}></input></div>
+    <div>Phone:<input type="number" name='phone' value={phone} onChange={(e)=>setPhone(e.target.value)}></input> </div>
+    <div>password:<input type="password" name='password' value={password} onChange={(e)=>setPassword(e.target.value)}></input></div>
+    <div><button type="submit">Submit</button>
     <button type="reset">Clear</button></div>
     </form>
     </>
--- src/menubar/Service.jsx
+++ src/menubar/Service.jsx
@@ -1,9 +1,56 @@
-import React from 'react'
+import React from 'react';
 
 const Service = () => {
-  return (
-    <div>Service</div>
-  )
-}
 
-export default Service
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    alert("Form Submitted Successfully");
+  };
+
+  return (
+    <>
+      <h1>Service Form</h1>
+
+      <form onSubmit={handleSubmit}>
+        <div>
+          Name:
+          <input type="text" name="name" required />
+        </div>
+
+        <div>
+          <h2>Gender:</h2>
+
+          <label>
+            <input type="radio" name="gender" value="Male" />
+            Male
+          </label>
+
+          <label>
+            <input type="radio" name="gender" value="Female" />
+            Female
+          </label>
+        </div>
+
+        <div>
+          <h2>Issue:</h2>
+
+          <label>
+            <input type="checkbox" name="issue" value="Technical" />
+            Technical Issue
+          </label>
+        </div>
+
+        <div>
+          Service:
+          <input type="text" name="service" />
+        </div>
+
+        <br />
+
+        <button type="submit">Submit</button>
+      </form>
+    </>
+  );
+};
+
+export default Service;
